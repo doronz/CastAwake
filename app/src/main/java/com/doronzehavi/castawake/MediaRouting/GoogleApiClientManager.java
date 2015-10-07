b4f9978c6c4d7a617fa21c15df1144a796d5420b
Doron <devdoron@gmail.com>
2015-10-07 00:12:59 -0700

    Casting works! But can't yet disconnect.

---
+++ app/src/main/java/com/doronzehavi/castawake/MediaRouting/GoogleApiClientManager.java
@@ -0,0 +1,174 @@
+package com.doronzehavi.castawake.MediaRouting;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.media.MediaRouter;
+
+import com.doronzehavi.castawake.Constants;
+import com.doronzehavi.castawake.LogUtils;
+import com.google.android.gms.cast.ApplicationMetadata;
+import com.google.android.gms.cast.Cast;
+import com.google.android.gms.cast.CastDevice;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+
+public class GoogleApiClientManager {
+
+    private final Context mContext;
+    private ApiConnectionCallbacks mConnectionCallbacks;
+    private ApiConnectionFailedListener mConnectionFailedListener;
+    private Cast.Listener mCastListener;
+    private CastDevice mSelectedDevice;
+    private GoogleApiClient mApiClient;
+    private String mSessionId;
+    private boolean mApplicationStarted;
+    private boolean mWaitingForReconnect;
+    private MediaRouter mMediaRouter;
+
+    public GoogleApiClientManager(Context context, MediaRouter router) {
+        mContext = context;
+        mMediaRouter = router;
+    }
+
+    public void setSelectedDevice(CastDevice selectedDevice) {
+        mSelectedDevice = selectedDevice;
+    }
+
+    /**
+     * Start the receiver app
+     */
+    public void launchReceiver() {
+        try {
+            mCastListener = new Cast.Listener() {
+
+                @Override
+                public void onApplicationDisconnected(int errorCode) {
+                    LogUtils.d("application has stopped");
+                    teardown(true);
+                }
+
+            };
+
+            // Connect to Google Play services
+            mConnectionCallbacks = new ApiConnectionCallbacks();
+            mConnectionFailedListener = new ApiConnectionFailedListener();
+
+            Cast.CastOptions.Builder apiOptionsBuilder = Cast.CastOptions
+                    .builder(mSelectedDevice, mCastListener);
+            mApiClient = new GoogleApiClient.Builder(mContext)
+                    .addApi(Cast.API, apiOptionsBuilder.build())
+                    .addConnectionCallbacks(mConnectionCallbacks)
+                    .addOnConnectionFailedListener(mConnectionFailedListener)
+                    .build();
+
+            mApiClient.connect();
+        } catch (Exception e) {
+            LogUtils.e("Failed launchReceiver", e);
+        }
+    }
+
+    /**
+     * Tear down the connection to the receiver
+     */
+    public void teardown(boolean selectDefaultRoute) {
+        LogUtils.d("teardown");
+        if (mApiClient != null) {
+            if (mApplicationStarted) {
+                if (mApiClient.isConnected() || mApiClient.isConnecting()) {
+                    Cast.CastApi.stopApplication(mApiClient, mSessionId);
+                    mApiClient.disconnect();
+                }
+                mApplicationStarted = false;
+            }
+            mApiClient = null;
+        }
+        if (selectDefaultRoute) {
+            mMediaRouter.selectRoute(mMediaRouter.getDefaultRoute());
+        }
+        mSelectedDevice = null;
+        mWaitingForReconnect = false;
+        mSessionId = null;
+    }
+
+    private class ApiConnectionCallbacks implements
+            GoogleApiClient.ConnectionCallbacks {
+        private boolean mWaitingForReconnect;
+
+        @Override
+        public void onConnected(Bundle connectionHint) {
+            LogUtils.d("onConnected");
+
+            if (mApiClient == null) {
+                // We got disconnected while this runnable was pending
+                // execution.
+                return;
+            }
+            try {
+                if (mWaitingForReconnect) {
+                    mWaitingForReconnect = false;
+                    // Check if the receiver app is still running
+                    if ((connectionHint != null)
+                            && connectionHint.getBoolean(Cast.EXTRA_APP_NO_LONGER_RUNNING)) {
+                        LogUtils.d("App  is no longer running");
+                        teardown(true);
+                    } else {
+                        // Todo: Create/re-create custom message channel
+                    }
+                } else {
+                    // Launch the receiver app
+                    Cast.CastApi.launchApplication(mApiClient, Constants.APP_ID, false)
+                            .setResultCallback(
+                                    new ResultCallback<Cast.ApplicationConnectionResult>() {
+                                        @Override
+                                        public void onResult(
+                                                Cast.ApplicationConnectionResult result) {
+                                            Status status = result.getStatus();
+                                            LogUtils.d(
+                                                    "ApplicationConnectionResultCallback.onResult:"
+                                                            + status.getStatusCode());
+                                            if (status.isSuccess()) {
+                                                ApplicationMetadata applicationMetadata = result
+                                                        .getApplicationMetadata();
+                                                mSessionId = result.getSessionId();
+                                                String applicationStatus = result
+                                                        .getApplicationStatus();
+                                                boolean wasLaunched = result.getWasLaunched();
+                                                LogUtils.d("application name: "
+                                                        + applicationMetadata.getName()
+                                                        + ", status: " + applicationStatus
+                                                        + ", sessionId: " + mSessionId
+                                                        + ", wasLaunched: " + wasLaunched);
+                                                mApplicationStarted = true;
+
+                                            } else {
+                                                LogUtils.e("application could not launch");
+                                                teardown(true);
+                                            }
+                                        }
+                                    });
+                }
+            } catch (Exception e) {
+                LogUtils.e("Failed to launch application", e);
+            }
+        }
+
+        @Override
+        public void onConnectionSuspended(int cause) {
+            LogUtils.d("onConnectionSuspended");
+            mWaitingForReconnect = true;
+        }
+    }
+
+    private class ApiConnectionFailedListener implements
+            GoogleApiClient.OnConnectionFailedListener {
+        @Override
+        public void onConnectionFailed(ConnectionResult result) {
+            teardown(false);
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/doronzehavi/castawake/MediaRouting/MediaRouterManager.java
@@ -0,0 +1,73 @@
+package com.doronzehavi.castawake.MediaRouting;
+
+
+import android.content.Context;
+import android.support.v7.media.MediaControlIntent;
+import android.support.v7.media.MediaRouteSelector;
+import android.support.v7.media.MediaRouter;
+import android.support.v7.media.MediaRouter.RouteInfo;
+
+import com.doronzehavi.castawake.Constants;
+import com.google.android.gms.cast.CastDevice;
+import com.google.android.gms.cast.CastMediaControlIntent;
+
+public class MediaRouterManager {
+
+
+    private final MediaRouteSelector mSelector;
+    private final MediaRouter mMediaRouter;
+    private final MediaRouterCallback mCallback;
+    private final Context mContext;
+    private GoogleApiClientManager mApiManager;
+
+    public MediaRouterManager(Context context) {
+        mContext = context;
+        mMediaRouter = MediaRouter.getInstance(mContext.getApplicationContext());
+
+        // Create a route selector for the type of routes your app supports.
+        mSelector = new MediaRouteSelector.Builder()
+                // These are the framework-supported intents
+                .addControlCategory(MediaControlIntent.CATEGORY_LIVE_AUDIO)
+                .addControlCategory(MediaControlIntent.CATEGORY_LIVE_VIDEO)
+                .addControlCategory(MediaControlIntent.CATEGORY_REMOTE_PLAYBACK)
+                .addControlCategory(CastMediaControlIntent.categoryForCast(Constants.APP_ID))
+                .build();
+
+        mCallback = new MediaRouterCallback();
+
+        mApiManager = new GoogleApiClientManager(mContext, mMediaRouter);
+    }
+
+    public MediaRouteSelector getSelector() {
+        return mSelector;
+    }
+
+
+    public void addCallback() {
+        mMediaRouter.addCallback(mSelector, mCallback,
+                MediaRouter.CALLBACK_FLAG_REQUEST_DISCOVERY);
+    }
+
+    public void removeCallback() {
+        mMediaRouter.removeCallback(mCallback);
+    }
+
+    private class MediaRouterCallback extends MediaRouter.Callback {
+
+        @Override
+        public void onRouteSelected(MediaRouter router, RouteInfo route) {
+            if (route.getPlaybackType() == RouteInfo.PLAYBACK_TYPE_LOCAL) {
+                return;
+            }
+            mApiManager.setSelectedDevice(CastDevice.getFromBundle(route.getExtras()));
+            mApiManager.launchReceiver();
+        }
+
+        @Override
+        public void onRouteUnselected(MediaRouter router, RouteInfo route) {
+            mApiManager.setSelectedDevice(null);
+        }
+    }
+
+
+}
